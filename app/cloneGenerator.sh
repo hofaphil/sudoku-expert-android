bbd7d37e80d5ff4c793bb321ef5342cdd56fd60f
Philipp Hofer <philipp.hofer@aol.com>
2022-03-21 15:18:08 +0100

    moved cone script

--- app/cloneGenerator.sh
+++
@@ -1,4 +0,0 @@
-git clone git@github.com:hofaphil/sudoku-expert-generator.git src/main/cpp/temp
-mkdir src/main/cpp/generator
-mv src/main/cpp/temp/src/* src/main/cpp/generator
-rm -rf src/main/cpp/temp
