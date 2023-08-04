937baf72e0899abb81f0a1cd8ed5d8e5c789907d
Philipp Hofer <philipp.hofer@aol.com>
2023-08-04 19:18:30 +0200

    update clone script with https

--- cloneGenerator.sh
+++ cloneGenerator.sh
@@ -1,4 +1,4 @@
-git clone git@github.com:hofaphil/sudoku-expert-generator.git app/src/main/cpp/temp
+git clone https://github.com/hofaphil/sudoku-expert-generator.git app/src/main/cpp/temp
 mkdir app/src/main/cpp/generator
 mv app/src/main/cpp/temp/src/* app/src/main/cpp/generator
 rm -rf app/src/main/cpp/temp
