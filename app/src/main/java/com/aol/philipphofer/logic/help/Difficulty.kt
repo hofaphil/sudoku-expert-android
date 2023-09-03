7739fab82b0c35dcaa6abf995d5bb983f6885b96
Philipp Hofer <philipp.hofer@aol.com>
2023-09-03 15:23:56 +0200

    first refactoring to kotlin

---
+++ app/src/main/java/com/aol/philipphofer/logic/help/Difficulty.kt
@@ -0,0 +1,27 @@
+package com.aol.philipphofer.logic.help
+
+import android.content.Context
+import com.aol.philipphofer.R
+
+enum class Difficulty(
+    val number: Int,
+    val text: Int,
+    val freeFields: Int
+) {
+
+    BEGINNER(0, R.string.beginner, 42),
+    ADVANCED(1, R.string.advanced, 49),
+    EXPERT(2, R.string.expert, 56);
+
+    fun getText(context: Context): String {
+        return context.getString(text)
+    }
+}
+
+fun getDifficulty(number: Int): Difficulty {
+    return when (number) {
+        0 -> Difficulty.BEGINNER
+        2 -> Difficulty.EXPERT
+        else -> Difficulty.ADVANCED
+    }
+}
