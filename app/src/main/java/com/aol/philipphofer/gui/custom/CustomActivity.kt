4e549427c977e79acbf5ec4beef260f2128a71d4
Philipp Hofer <philipp.hofer@aol.com>
2023-09-11 22:02:16 +0200

    rewrite custom activity to kotlin

---
+++ app/src/main/java/com/aol/philipphofer/gui/custom/CustomActivity.kt
@@ -0,0 +1,16 @@
+package com.aol.philipphofer.gui.custom
+
+import android.app.Activity
+import android.os.Bundle
+import com.aol.philipphofer.persistence.Data
+
+abstract class CustomActivity : Activity() {
+
+    protected lateinit var data: Data
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        data = Data.instance(this);
+        setTheme(data.theme)
+    }
+}
